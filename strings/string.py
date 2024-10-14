821eeda9850cf8c6227ab301561da2a6e2f2c38f
w3cj <themondaycup@gmail.com>
2024-10-13 23:06:39 -0400

    added string section

---
+++ strings/string.py
@@ -0,0 +1,9 @@
+#simple string
+fact = "The moon has no atmospher."
+print(fact)
+
+#immutability of string: meaning they cant change
+#adding a new string to the return value will give a new result
+fact = "The moon has no atmosphere."
+two_facts = fact + " No sound can be heard on the Moon."
+print(two_facts)
